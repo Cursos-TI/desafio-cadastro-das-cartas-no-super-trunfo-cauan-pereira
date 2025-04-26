1febf76c3d64041ae0f937341f1ece93c576e92a
cauan-pereira <programacao.cauanpereira@gmail.com>
2025-04-26 00:27:04 +0000

    Corrigi os erros de sintaxe

--- DesafioCartas/DesafioCartas.c
+++ DesafioCartas/DesafioCartas.c
@@ -1,58 +1,89 @@
 #include <stdio.h>
 
+    //Carta 1
     int main(){
-        char estado;
-        char codigo[3];
-        char nome;
-        int populacao;
-        float area;
-        float pib;
-        int turisticos;
+        char estado1;            //Para escolher a letra que representa o estado escolhido 
+        char codigo1[4];         //Para escolher o código da carta
+        char nome1[20];
+        int populacao1;
+        float area1;
+        float pib1;
+        int pontos_turisticos1;
 
-        printf("Escolha uma letra entre 'A' e 'H' para sua carta 1");
-        scanf("%s", &estado);
+    //Carta 2    
+        char estado2;            //Para escolher o estado 
+        char codigo2[4];
+        char nome2[20];
+        int populacao2;
+        float area2;
+        float pib2;
+        int pontos_turisticos2;
+
+    //Entrada de dados da carta 1
+
+        printf("Carta 1 - Escolha uma letra entre 'A' e 'H' que represente seu estado: \n");
+        scanf(" %c", &estado1);
 
         printf("Vamos criar o código de sua carta. Insira a letra do estado seguida de um número de 01 a 04: \n");
-        scanf("%s", &codigo);
+        scanf("%s", &codigo1);
 
         printf("Insira o nome da cidade: \n");
-        scanf("%s", &nome);
+        scanf("%s", &nome1);
 
         printf("Insira a população da cidade: \n");
-        scanf("%d", &populacao);
+        scanf("%d", &populacao1);
 
         printf(" Insira a área da sua cidade: \n");
-        scanf("%f", &area);
+        scanf("%f", &area1);
 
         printf(" Insira o PIB da sua cidade: \n");
-        scanf("%f", &pib);
+        scanf("%f", &pib1);
 
         printf(" Insira o número de pontos turísticos da sua cidade: \n");
-        scanf("%d", &pib);
+        scanf("%d", &pontos_turisticos1);
+
+    //Entrada de dados da carta 2
 
         printf("Agora a carta 2. Escolha uma letra entre 'A' e 'H' para sua carta.");
-        scanf("%s", &estado);
+        scanf(" %c", &estado2);
 
         printf("Vamos criar o código de sua carta. Insira a letra do estado seguida de um número de 01 a 04: \n");
-        scanf("%s", &codigo);
+        scanf("%s", &codigo2);
 
         printf("Insira o nome da cidade: \n");
-        scanf("%s", &nome);
+        scanf("%s", &nome2);
 
         printf("Insira a população da cidade: \n");
-        scanf("%d", &populacao);
+        scanf("%d", &populacao2);
 
         printf(" Insira a área da sua cidade: \n");
-        scanf("%f", &area);
+        scanf("%f", &area2);
 
         printf(" Insira o PIB da sua cidade: \n");
-        scanf("%f", &pib);
+        scanf("%f", &pib2);
 
         printf(" Insira o número de pontos turísticos da sua cidade: \n");
-        scanf("%d", &pib);
+        scanf("%d", &pontos_turisticos2);
 
-        printf("Nome do aluno: %s - Matrícula: %d", nome, matricula);
-        printf("Idade: %d - Altura: %f", idade, altura);
+    //Impressão das informações
+
+        printf("\n===== CARTA 1 =====\n");
+        printf("Estado: %c", estado1);
+        printf("Código: %s", codigo1);
+        printf("Cidade: %s", nome1);
+        printf("População: %d", populacao1);
+        printf("Área: %f", area1);
+        printf("PIB: %f", pib1);
+        printf("Pontos turísticos: %d", pontos_turisticos1);
+
+        printf("\n===== CARTA 2 =====\n");
+        printf("Estado: %c", estado1);
+        printf("Código: %s", codigo1);
+        printf("Cidade: %s", nome1);
+        printf("População: %d", populacao1);
+        printf("Área: %f", area1);
+        printf("PIB: %f", pib1);
+        printf("Pontos turísticos: %d", pontos_turisticos1);
 
         return 0;
     }
