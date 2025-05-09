d0873da16db904321a286d5029794a655ad174fc
cauan-pereira <programacao.cauanpereira@gmail.com>
2025-05-09 00:52:51 +0000

    Adicionei a densidade populacional e o PIB per capita

--- DesafioCartas/DesafioCartas.c
+++ DesafioCartas/DesafioCartas.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 
     //Variáveis da Carta 1
-    int main(){
-        char estado1;               //Armazena a letra que represetna o estado     
-        char codigo1[4];            //Armazena o código que representa o estado
-        char nome1[20];             //Armazena o nome da cidade
-        int populacao1;             //Armazena o número que representa a população da cidade
-        float area1;                //Armazena o número que representa a área da cidade
-        float pib1;                 //Armazena o número que representa o PIB da cidade
-        int pontos_turisticos1;     //Armazena a quantidade de pontos turísticos da cidade
+    int main(){ 
+        char estado1;                                      //Armazena a letra que represetna o estado     
+        char codigo1[4];                                   //Armazena o código que representa o estado
+        char nome1[20];                                    //Armazena o nome da cidade
+        int populacao1;                                    //Armazena o número que representa a população da cidade
+        float area1;                                       //Armazena o número que representa a área da cidade
+        float pib1;                                        //Armazena o número que representa o PIB da cidade
+        int pontos_turisticos1;                            //Armazena a quantidade de pontos turísticos da cidade
+        float densidade_populacional1 = populacao1/area1;  //Armazena a densidade populacional da cidade dividindo a população dela pela sua área
+        float pib_per_capita1 = pib1/populacao1;            //Armazena o PIB Per Capita divindo o PIB pela população da cidade
 
     //Variáveis da Carta 2    
-        char estado2;               //Armazena a letra que represetna o estado     
-        char codigo2[4];            //Armazena o código que representa o estado
-        char nome2[20];             //Armazena o nome da cidade
-        int populacao2;             //Armazena o número que representa a população da cidade
-        float area2;                //Armazena o número que representa a área da cidade
-        float pib2;                 //Armazena o número que representa o PIB da cidade
-        int pontos_turisticos2;     //Armazena a quantidade de pontos turísticos da cidade
+        char estado2;                                      //Armazena a letra que represetna o estado     
+        char codigo2[4];                                   //Armazena o código que representa o estado
+        char nome2[20];                                    //Armazena o nome da cidade
+        int populacao2;                                    //Armazena o número que representa a população da cidade
+        float area2;                                       //Armazena o número que representa a área da cidade
+        float pib2;                                        //Armazena o número que representa o PIB da cidade
+        int pontos_turisticos2;                            //Armazena a quantidade de pontos turísticos da cidade
+        float densidade_populacional2 = populacao2/area2;  //Armazena a densidade populacional da cidade dividindo a população dela pela sua área
+        float pib_per_capita2 = pib2/populacao2;            //Armazena o PIB Per Capita divindo o PIB pela população da cidade
 
     //Entrada de dados da carta 1
 
@@ -75,6 +79,8 @@
         printf("Área: %.2f Km²\n", area1);
         printf("PIB: R$ %.2f bilhões\n", pib1);
         printf("Pontos turísticos: %d\n", pontos_turisticos1);
+        printf("Densidade Populacional: %.2f hab/km²\n", densidade_populacional1);
+        printf("PIB Per Capita: R$ %.2f bilhões\n", pib_per_capita1);
 
         printf("\n===== CARTA 2 =====\n");
         printf("Estado: %c\n", estado2);
@@ -83,7 +89,10 @@
         printf("População: %d\n", populacao2);
         printf("Área: %.2f Km²\n", area2);
         printf("PIB: R$ %.2f bilhões\n", pib2);
-        printf("Pontos turísticos: %d\n", pontos_turisticos2);
+        printf("Pontos turísticos: %f\n", pontos_turisticos2);
+        printf("Densidade Populacional: %.2f hab/km²\n", densidade_populacional2);
+        printf("PIB Per Capita: R$ %.2f bilhões\n", pib_per_capita2);
+        
 
         return 0;
     }
