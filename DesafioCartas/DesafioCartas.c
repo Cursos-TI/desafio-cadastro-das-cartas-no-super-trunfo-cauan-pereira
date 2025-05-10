44c96bad1456e6abd60064b8b398af9ac9d5c18b
cauan-pereira <programacao.cauanpereira@gmail.com>
2025-05-10 02:21:41 +0000

    Adicionei os superpoderes e a comparação entre as cartas

--- DesafioCartas/DesafioCartas.c
+++ DesafioCartas/DesafioCartas.c
@@ -11,6 +11,7 @@
         int pontos_turisticos1;                            //Armazena a quantidade de pontos turísticos da cidade
         float densidade_populacional1;                     //Armazena a densidade populacional da cidade dividindo a população dela pela sua área
         float pib_per_capita1;                             //Armazena o PIB Per Capita divindo o PIB pela população da cidade
+        float super_poder1;                                //Armazena o super poder da carta 
 
     //Variáveis da Carta 2    
         char estado2;                                      //Armazena a letra que represetna o estado     
@@ -20,8 +21,9 @@
         float area2;                                       //Armazena o número que representa a área da cidade
         float pib2;                                        //Armazena o número que representa o PIB da cidade
         int pontos_turisticos2;                            //Armazena a quantidade de pontos turísticos da cidade
-         float densidade_populacional2;                     //Armazena a densidade populacional da cidade dividindo a população dela pela sua área
+        float densidade_populacional2;                     //Armazena a densidade populacional da cidade dividindo a população dela pela sua área
         float pib_per_capita2;                             //Armazena o PIB Per Capita divindo o PIB pela população da cidade
+        float super_poder2;                                //Armazena o super poder da carta 
 
     //Entrada de dados da carta 1
 
@@ -48,6 +50,7 @@
 
     densidade_populacional1 = populacao1 / area1;
     pib_per_capita1 = pib1 / populacao1;
+    super_poder1 = populacao1 + area1 + pib1 + pontos_turisticos1 + pib_per_capita1 + (1 / densidade_populacional1);
 
     //Entrada de dados da carta 2
 
@@ -74,6 +77,7 @@
 
         densidade_populacional2 = populacao2 / area2;
         pib_per_capita2 = pib2 / populacao2;
+        super_poder2 = populacao2 + area2 + pib2 + pontos_turisticos2 + pib_per_capita2 + (1 / densidade_populacional2);
 
     //Impressão das informações das duas cartas
 
@@ -87,6 +91,7 @@
         printf("Pontos turísticos: %d\n", pontos_turisticos1);
         printf("Densidade Populacional: %.2f hab/km²\n", densidade_populacional1);
         printf("PIB Per Capita: R$ %.2f bilhões\n", pib_per_capita1);
+        printf("Super Poder: %.2f\n", super_poder1);
 
         printf("\n===== CARTA 2 =====\n");
         printf("Estado: %c\n", estado2);
@@ -98,7 +103,19 @@
         printf("Pontos turísticos: %f\n", pontos_turisticos2);
         printf("Densidade Populacional: %.2f hab/km²\n", densidade_populacional2);
         printf("PIB Per Capita: R$ %.2f bilhões\n", pib_per_capita2);
+        printf("Super Poder: %.2f\n", super_poder2);
         
+    //Comparação dos atributos
+    
+        printf("\n===== COMPARAÇÃO DE CARTAS =====\n");
+        printf("População: Carta %d venceu (%d)\n", (populacao1 > populacao2) ? 1 : 2, (populacao1 > populacao2) ? 1 : 0);
+        printf("Área: Carta %d venceu (%d)\n", (area1 > area2) ? 1 : 2, (area1 > area2) ? 1 : 0);
+        printf("PIB: Carta %d venceu (%d)\n", (pib1 > pib2) ? 1 : 2, (pib1 > pib2) ? 1 : 0);
+        printf("Pontos Turísticos: Carta %d venceu (%d)\n", (pontos_turisticos1 > pontos_turisticos2) ? 1 : 2, (pontos_turisticos1 > pontos_turisticos2) ? 1 : 0);
+        printf("Densidade Populacional: Carta %d venceu (%d)\n", (densidade_populacional1 < densidade_populacional2) ? 1 : 2, (densidade_populacional1 < densidade_populacional2) ? 1 : 0);
+        printf("PIB per Capita: Carta %d venceu (%d)\n", (pib_per_capita1 > pib_per_capita2) ? 1 : 2, (pib_per_capita1 > pib_per_capita2) ? 1 : 0);
+        printf("Super Poder: Carta %d venceu (%d)\n", (super_poder1 > super_poder2) ? 1 : 2, (super_poder1 > super_poder2) ? 1 : 0);
+
 
         return 0;
     }
